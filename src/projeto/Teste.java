8d83e1dae48c00147fab4e85b3ea18b485a0f7c6
wellington <wellington_coimbra@outlook.com>
2017-03-29 15:15:03 -0300

    teste commit testeGit1

--- src/projeto/Teste.java
+++ src/projeto/Teste.java
@@ -1,10 +1,15 @@
 package projeto;
 
+import java.util.Iterator;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str="teste";
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.substring(0,i));
+			
+		}
 	}
 
 }
