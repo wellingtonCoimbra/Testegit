10c9c81446e98125793bc80b6e8caf6b65196959
wellington <wellington_coimbra@outlook.com>
2017-03-29 15:18:16 -0300

    teste commit TesteGit2

--- src/projeto/Teste.java
+++ src/projeto/Teste.java
@@ -3,7 +3,11 @@ package projeto;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int cont;
+		cont = 10;
+		for (int i = 0; i < cont; i++) {
+			System.out.println(i);
+		}
 
 	}
 
